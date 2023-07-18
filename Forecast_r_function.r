92ee5ee8d4f26ba0f00c913137ec83f050d49dc0
fahygao <fahygao@gmail.com>
2023-07-18 14:43:00 -0400

    add new changes

---
+++ Forecast_r_function.r
@@ -0,0 +1,7 @@
+library(forecast)
+Forecast_r_function= function(actuals, freq){
+    y <- ts(actuals,frequency = freq)
+    fit <- auto.arima(y, seasonal=TRUE)
+    forecasted = forecast(fit, h=5)
+    return (forecasted)
+    }
